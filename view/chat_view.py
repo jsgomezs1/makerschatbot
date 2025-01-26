2fc37d26c66adff43250129f0dcc0f7ef09de4c8
Kemjiga <kemerjg@gmail.com>
2025-01-26 18:10:30 +0100

    feat: add backend integration

--- view/chat_view.py
+++ view/chat_view.py
@@ -1,18 +1,38 @@
 import gradio as gr
+import requests
 
-def send_message(message, chat_history):
+def send_message(message, chat_history, chat_id=None):
+    backend_url = "http://127.0.0.1:8000"
+    user_url = f"{backend_url}/core/users/"
+
+    response = requests.get(user_url)
+    if response.status_code == 200:
+        user_id = response.json()[0]["id"]
+    else:
+        return
+
+    chat_url = f"{backend_url}/chat/claude"
+    payload = {
+        "prompt": message,
+        "created_by": user_id
+    }
+
+    if chat_id:
+        payload["chat_id"] = chat_id
+
+    print(payload)
+    headers = {"Content-Type": "application/json"}
+    response = requests.post(chat_url, json=payload, headers=headers)
+    if response.status_code == 200:
+        bot_reply = response.json()["response"]
+    else:
+        bot_reply = f"Error:{response}"
+
+
     chat_history.append(
         gr.ChatMessage(role="user", content=message)
     )
-
-    # Call endpoint or function to generate conversation
-    if message.strip().lower() == "hello":
-        bot_reply = "Hi there! How can I help you today?"
-    else:
-        bot_reply = f"You said: {message}"
     
-    
-    # Add user message and bot reply to the chat history
     chat_history.append(
         gr.ChatMessage(role="assistant", content=bot_reply)
     )
@@ -23,8 +43,8 @@ def send_message(message, chat_history):
 with gr.Blocks() as demo:
     gr.Markdown("## AI-Powered Chatbot")
     chat = gr.Chatbot(label="AI Inventory bot", type="messages")
-    message_input = gr.Textbox(placeholder="Type your message here...")
-    reload_button = gr.Button("New conversation")
+    message_input = gr.Textbox(label="Ask something",placeholder="Type your message here...")
+    reload_button = gr.Button("Clear chat")
     
     # Gradio state to matain chat history
     chat_history = gr.State([])
